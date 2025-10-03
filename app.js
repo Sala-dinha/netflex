4e39337337279f00a125627e1b420d824ed65669
ZeldaSuino <aleatoriamente00000007@gmail.com>
2025-10-03 09:45:53 -0300

    removidos comentários

--- app.js
+++ app.js
@@ -1,13 +1,6 @@
 const express = require('express');
-//TRAGO O OBJETO PARA SER UTILIZADO NAS MINHAS REQUISIÇÕES
 
-const Movie = require('./model/movie')
-const { where } = require('sequelize');
-
-//SERVIDOR DE APLICAÇÃO
 const app = express();
-
-//INFORMO QUE AS REQUISIÇÕES QUE FOREM FEITAS, ESTARÃO NO FORMATO JSON
 
 const user_router = require('./routers/user_router');
 app.use('/user', user_router);

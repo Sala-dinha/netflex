9e292d093578248c3986ad0d4f53306ff642ed5a
sala-dinha <sara.sampaio.tinfem2023@gmail.com>
2025-12-11 18:07:35 -0300

    metemo o handlebars

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 const express = require('express');
+const { engine } = require('express-handlebars');
 
 const app = express();
+
+app.engine('handlebars', engine({defaultLayout:'main'}));
+app.set('view engine', 'handlebars');
+app.use(express.static('public'))
 
 association = require('./association/association')
 
